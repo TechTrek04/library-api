67eebff85e3aabc41f3fd20f8948ba330c9c056d
ohenebaopare <ohenebaopareok@gmail.com>
2025-03-11 15:18:21 +0000

    Update library_model.js

--- models/library_model.js
+++ models/library_model.js
@@ -4,7 +4,7 @@ const bookSchema = new Schema({
     title:{type: String, required: true},
     author:{type: String, required: true},
     genre:{type: String, required: true},
-    publishedYear:{type: Number, required: true},
+    yearPublished:{type: Number, required: true},
     description:{type: String, required: true},
     pages:{type: Number, required: true},
 
